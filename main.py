6420b1b0744dd88d32fd6e86af1c867b6c3887d7
Phuong Khanh Pham <phuong-khanh.pham@epita.fr>
2024-03-27 18:18:54 +0100

    Last change

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import chevron
 import Map
 
+
 def render_map(json_file):
 
     map = Map.Map.LoadFromGeoJson(json_file)
@@ -10,15 +11,28 @@ def render_map(json_file):
     (x1, y1, x2, y2) = map.bounding_box()
 
     data = {
-        "classes": [Map.Building, Map.District, Map.Road, Map.Wall, Map.Plank, Map.Prism, 
-                    Map.Square, Map.Green, Map.Field, Map.Tree, Map.Earth, Map.Water, Map.River],
+        "classes": [
+            Map.Building,
+            Map.District,
+            Map.Road,
+            Map.Wall,
+            Map.Plank,
+            Map.Prism,
+            Map.Square,
+            Map.Green,
+            Map.Field,
+            Map.Tree,
+            Map.Earth,
+            Map.Water,
+            Map.River,
+        ],
         "bbox": {
-            "x": x1, 
+            "x": x1,
             "y": y1,
             "width": x2 - x1,
             "height": y2 - y1,
         },
-        "items": map.items
+        "items": map.items,
     }
 
     output = chevron.render(open("map-template.svg"), data)
@@ -27,7 +41,12 @@ def render_map(json_file):
     print(f"Generated {svg_file}")
 
 
-path = Path(sys.argv[1])
-filelists = [file for file in path.iterdir() if file.suffix == ".json"]
-for file in filelists:
-    render_map(file)
+def main():
+    path = Path(sys.argv[1])
+    filelists = [file for file in path.iterdir() if file.suffix == ".json"]
+    for file in filelists:
+        render_map(file)
+
+
+if __name__ == "__main__":
+    main()
